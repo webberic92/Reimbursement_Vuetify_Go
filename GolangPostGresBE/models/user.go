fdf0b5662f55c8dd91dc8c18425ec1d8c03d99ba
unknown <webberic92@yahoo.com>
2021-08-24 16:44:36 -0400

    Updated go user struct

--- GolangPostGresBE/models/user.go
+++ GolangPostGresBE/models/user.go
@@ -1,8 +1,12 @@
 package models
 
 type User struct {
-	Id       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"`
+	Id          uint   `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	PhoneNumber int    `json:"phoneNumber"`
+	Email       string `json:"email" gorm:"unique"`
+	UserType    string `json:"userType"`
+	Sou         bool   `json:"sou"`
+	Password    []byte `json:"-"`
 }
